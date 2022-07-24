9e77ace84713477c176eab9be812b7a83db841fb
Hesam Abbasi <hesam.abbasi011@gmail.com>
2022-07-24 11:26:51 +0430

    add files to subfolders

--- Script/ragham-project.js
+++ Script/ragham-project.js
@@ -14,7 +14,7 @@ function InfoTabFirstButton () {
     document.getElementById("second-button").style.color = "#333333";
     document.getElementById("third-button").style.backgroundColor = "#edeaea";
     document.getElementById("third-button").style.color = "#333333";
-    document.getElementById("info-third-part-image").src = "info-third-part-image-first-button.png"
+    document.getElementById("info-third-part-image").src = "Images/info-third-part-image-first-button.png"
     document.getElementById("info-third-part-heading").innerHTML = "شریکی کار می کنی یا تنهایی؟";
     document.getElementById("info-third-part-paragraph").innerHTML = "رقم را روی دستگاه شرکا هم نصب کن و صورت حساب ها را با هر چند نفر لازم داشته باشی به راحتی به اشتراک بگذار";
 }
@@ -25,7 +25,7 @@ function InfoTabSecondButton () {
     document.getElementById("first-button").style.color = "#333333";
     document.getElementById("third-button").style.backgroundColor = "#edeaea";
     document.getElementById("third-button").style.color = "#333333";
-    document.getElementById("info-third-part-image").src = "info-third-part-image-second-button.png"
+    document.getElementById("info-third-part-image").src = "Images/info-third-part-image-second-button.png"
     document.getElementById("info-third-part-heading").innerHTML = "تعمیرات، نصب یا راه اندازی؟";
     document.getElementById("info-third-part-paragraph").innerHTML = "برای تمام کارهای خدماتی می توانی حساب کتاب مشتری هایت را با رقم ثبت و نگهداری کنی";
 }
@@ -36,7 +36,7 @@ function InfoTabThirdButton () {
     document.getElementById("first-button").style.color = "#333333";
     document.getElementById("second-button").style.backgroundColor = "#edeaea";
     document.getElementById("second-button").style.color = "#333333";
-    document.getElementById("info-third-part-image").src = "info-third-part-image-third-button.png"
+    document.getElementById("info-third-part-image").src = "Images/info-third-part-image-third-button.png"
     document.getElementById("info-third-part-heading").innerHTML = "اقساطی یا نقدی؟";
     document.getElementById("info-third-part-paragraph").innerHTML = "هیچ فرقی نمی کند که چطوری کار می کنی. با رقم می توانی معاملات خرید و فروش ها را به راحتی پیگیری کنی";
 }
